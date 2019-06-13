fcf7efe102c721b56215bda9bce8aa4fcf6913f4
Winton Ou <winton.ou@cargosmart.com>
2019-06-13 15:10:08 +0800

    eureka enable

--- src/main/java/com/dmall/orderservice/OrderServiceApplication.java
+++ src/main/java/com/dmall/orderservice/OrderServiceApplication.java
@@ -2,11 +2,14 @@ package com.dmall.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-//TODO: enable feign client
+@EnableEurekaClient
+@EnableFeignClients
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
